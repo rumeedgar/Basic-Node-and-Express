552674e80c7a1b6d0f106f32c256623101bd5951
rumeedgar <rumeedgar@icloud.com>
2022-08-07 13:35:28 +0530

    Serve json on a particular route

--- myApp.js
+++ myApp.js
@@ -7,10 +7,17 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+//Serving static assets
 app.use(express.static(__dirname + '/public/'));
 app.use('/public', express.static(__dirname + '/public'));
 
-
+//Serve json on a specific route
+app.get(/json/, (req, res) => 
+{
+    res.json(
+        { "message" : "Hello json"}
+    );
+});
 
 
 
