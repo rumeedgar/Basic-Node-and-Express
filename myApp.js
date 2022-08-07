652521d651342e5ec6a005a3015577af993d8eb0
rumeedgar <rumeedgar@icloud.com>
2022-08-07 14:17:42 +0530

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 console.log('Hello World');
@@ -11,15 +12,26 @@ app.get('/', (req, res) => {
 app.use(express.static(__dirname + '/public/'));
 app.use('/public', express.static(__dirname + '/public'));
 
-//Serve json on a specific route
-app.get(/json/, (req, res) => 
-{
-    res.json(
-        { "message" : "Hello json"}
-    );
+// //Serve json on a specific route
+// app.get(/json/, (req, res) => 
+// {
+//     res.json(
+//         { "message" : "Hello json"}
+//     );
+// });
+
+app.get(/json/, (req, res) => {
+    console.log(process.env.MESSAGE_STYLE, " <= message style");
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json(
+            { "message": "HELLO JSON"}
+        )
+    } else {
+        res.json(
+            { "message": "Hello json"}
+        )
+    }
 });
-
-
 
 
 
