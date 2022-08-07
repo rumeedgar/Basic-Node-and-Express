8bae7fdde2b70f5cfa3e683285c87ff347fe1996
rumeedgar <rumeedgar@icloud.com>
2022-08-07 13:16:54 +0530

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -2,9 +2,13 @@ let express = require('express');
 let app = express();
 console.log('Hello World');
 // bGround.log('Hello World');
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
+
+app.use(express.static(__dirname + '/public/'));
+app.use('/public', express.static(__dirname + '/public'));
 
 
 
